b973ad0426576cfea3623c6552c94917275a0144
Andrew Roetker <andrew.roetker@puppetlabs.com>
2014-07-15 22:02:59 -0700

    Whip it real good

--- js/nertz.js
+++ js/nertz.js
@@ -4,7 +4,29 @@ var cardChars = {
     3 : { spec: '\u2663', color: 'black', suit: 'clubs' },
     4 : { spec: '\u2666', color: '#780000', suit: 'diamonds' },
 }
-var values = { 1: 'Ace', 11: 'Jack', 12: 'Queen', 13: 'King' }
+
+var values = {
+    1: 'Ace',
+    2: '2',
+    3: '3',
+    4: '4',
+    5: '5',
+    6: '6',
+    7: '7',
+    8: '8',
+    9: '9',
+    10: '10',
+    11: 'Jack',
+    12: 'Queen',
+    13: 'King'
+}
+
+function cardImg(suit, value) {
+    return $('<img style="z-index: 0;"' +
+             'src="http://openclipart.org/people/' +
+             'nicubunu/nicubunu_White_deck_' +
+             value + '_of_' + suit + '.svg" />')
+}
 
 function Board(object) {
     this.nertz  = object.Nertz;
@@ -12,18 +34,12 @@ function Board(object) {
     this.river  = object.River;
     this.show   = object.Show;
     this.render = function() {
-        var position;
         var pile = 0;
-        $('#boardGame div div img').remove();
+        $('#boardGame div img').remove();
         $.each( this.river, function( jndex, cards ) {
             $.each( cards, function( index, card ) {
                 if ( card.Value ) {
-                    var value = values[card.Value];
-                    if ( !value ) {
-                        value = card.Value;
-                    }
-                    var src = "/nicubunu_White_deck_" + value + "_of_" + cardChars[card.Suit].suit + ".svg";
-                    img = $('<img style="z-index: 0;" src="http://openclipart.org/people/nicubunu' + src + '" />');
+                    img = cardImg( cardChars[card.Suit].suit, values[card.Value] )
                     $('#pile' + pile).append(img);
                     pile += 1;
                     img.droppable({
@@ -40,43 +56,38 @@ function Board(object) {
                 }
             });
         });
+        var position;
         $.each( this.nertz, function( index, card ) {
             if ( card.Value ) {
-                var value = values[card.Value];
-                if ( !value ) {
-                    value = card.Value;
-                }
-                var src = "/nicubunu_White_deck_" + value + "_of_" + cardChars[card.Suit].suit + ".svg";
-                img = $('<img src="http://openclipart.org/people/nicubunu' + src + '" />');
+                img = cardImg( cardChars[card.Suit].suit, values[card.Value] )
                 $('#nertz').append(img);
-                if (index) {
-                    var offset;
-                    img.css( 'top', position.top + .5 ).css( 'left', position.left ).css( 'position', 'absolute' );
-                }
                 position = img.position();
+                if (index) {
+                    img.css( 'top', position.top + .5 )
+                       .css( 'left', position.left )
+                       .css( 'position', 'absolute' );
+                }
             }
         });
         $.each( this.stream, function( index, card ) {
             if ( card.Value ) {
                 img = $('<img src="http://openclipart.org/people/nicubunu/nicubunu_Card_backs_simple_blue.svg" />');
                 $('#stream').append(img);
-                if (index) {
-                    var offset;
-                    img.css( 'top', position.top ).css( 'left', position.left - .5).css( 'position', 'absolute' );
-                }
                 position = img.position();
+                if (index) {
+                    img.css( 'top', position.top )
+                       .css( 'left', position.left - .5)
+                       .css( 'position', 'absolute' );
+                }
             }
         });
         $.each( this.show, function( index, card ) {
             if ( card.Value ) {
-                var value = values[card.Value];
-                if ( !value ) {
-                    value = card.Value;
-                }
-                var src = "/nicubunu_White_deck_" + value + "_of_" + cardChars[card.Suit].suit + ".svg";
-                img = $('<img src="http://openclipart.org/people/nicubunu' + src + '" />');
+                img = cardImg( cardChars[card.Suit].suit, values[card.Value] )
                 $('#show').append(img);
-                img.draggable({start: function(event, ui) { ui.dragable.css('position', 'absolute').css('z-index', 9999) ; } });
+                img.draggable({
+                    zIndex: 100,
+                });
             }
         });
 
